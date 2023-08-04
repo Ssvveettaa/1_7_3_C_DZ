c5492691dfb2bd043b14187d7aa89d423ecd2db9
Svetlana Panina <paninasi@inbox.ru>
2023-08-04 04:29:01 +0300

    Task19v2

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,18 +6,23 @@
 // 12821 –> да
 // 23432 –> да
 
-string Palindrome(int num)
+bool FiveDigit(int num)
 {
     if (num < 0) num *= -1;
-    if (num >= 10000 && num <= 99999)
-    {
-        return num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10 ? "является палиндромом" : "не является палиндромом";
-    }
-    else return "не пятизначное";
-}
+    return num >= 10000 && num <= 99999;
+}   // Или только (return (num >= 10000 && num <= 99999) || (num <= -10000 && num >= -99999);) в теле метода.
+
+bool Palindrome(int fiveDigit)
+{
+    return fiveDigit / 10000 == fiveDigit % 10 && fiveDigit / 1000 % 10 == fiveDigit / 10 % 10;
+}   // Не привожу число сначала к модулю, так как метод работает и с отрицательными тоже.
 
 Console.Write("Введите целое пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-string palindrome = Palindrome(number);
-Console.WriteLine($"Число {number} {palindrome}.");
+if (FiveDigit(number))
+{
+    bool palindrome = Palindrome(number);
+    Console.WriteLine(palindrome ? $"Число {number} является палиндромом." : $"Число {number} не является палиндромом.");
+}
+else Console.WriteLine("Число не пятизначное.");
